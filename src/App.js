f26d4944ec5fb0dce9b6786981c98a520375f267
g-mahendra <mahendragharge2000@gmail.com>
2022-02-05 14:47:50 +0530

    [Mahendra] | ADD | adds logic for todomatic app tests

--- src/App.js
+++ src/App.js
@@ -1,25 +1,112 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  const [todo, setTodo] = useState("");
+  const [todoList, setTodoList] = useState([]);
+  const handleChange = (event) => setTodo(event.target.value);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setTodoList([
+      ...todoList,
+      {
+        id: `${todoList.length + 1}`,
+        value: todo,
+        isComplete: false,
+      },
+    ]);
+    setTodo("");
+  };
+
+  const handleCompleteTodo = (id) => {
+    const updatedTodos = todoList.map((todo) => {
+      if (todo.id === id) {
+        return {
+          ...todo,
+          completed: !todo.completed,
+        };
+      }
+      return todo;
+    });
+
+    setTodoList(updatedTodos);
+  };
+
+  const handleDeleteTodo = (id) => {
+    const updatedTodos = todoList.filter((todo) => id !== todo.id);
+    setTodoList(updatedTodos);
+  };
+
+  const List = (
+    <div id="todo-list" data-testid="list">
+      {todoList.map((item) => {
+        return (
+          <div id="todo-item" data-testid="listItem" key={item.id}>
+            {item.completed ? (
+              <>
+                <h3 data-testid="itemText">{item.value} completed</h3>
+                <div id="buttons-container">
+                  <button onClick={() => handleCompleteTodo(item.id)}>
+                    Mark as incomplete
+                  </button>
+                  <button onClick={() => handleDeleteTodo(item.id)}>
+                    Delete
+                  </button>{" "}
+                </div>
+              </>
+            ) : (
+              <>
+                <h3 data-testid="itemText">{item.value}</h3>
+                <div id="buttons-container">
+                  <button onClick={() => handleCompleteTodo(item.id)}>
+                    Mark as complete
+                  </button>
+                  <button onClick={() => handleDeleteTodo(item.id)}>
+                    Delete
+                  </button>{" "}
+                </div>
+              </>
+            )}
+          </div>
+        );
+      })}
     </div>
   );
-}
+
+  return (
+    <div className="container">
+      <div className="todobox">
+        <div className="heading">
+          <h1 data-testid="heading">Todomatic</h1>
+        </div>
+        <div className="todoinput-container">
+          <label htmlFor="input">Whats on your mind ?</label>
+          <form
+            className="input-and-submit-container"
+            data-testid="form"
+            onSubmit={handleSubmit}
+          >
+            <input
+              id="input"
+              className="todoinput"
+              placeholder="Enter todo item"
+              data-testid="todoInput"
+              value={todo}
+              onChange={handleChange}
+            />
+            <input
+              className="submit"
+              disabled={todo === ""}
+              data-testid="submit"
+              type="submit"
+              value="ADD Todo"
+            />
+          </form>
+        </div>
+        {List}
+      </div>
+    </div>
+  );
+};
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,89 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { fireEvent, getByTestId, render, within } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("if it renders heading", () => {
+  const { getByTestId } = render(<App />);
+
+  expect(getByTestId("heading")).toHaveTextContent("Todomatic");
+});
+
+test("if todo input is empty initially and submit button is disabled", () => {
+  const { getByTestId } = render(<App />);
+  const todoInput = getByTestId("todoInput");
+  const submitButton = getByTestId("submit");
+
+  expect(todoInput).toHaveValue("");
+  expect(submitButton).toBeDisabled();
+});
+
+test("if value is getting added to list after form submital and if input is getting cleared", () => {
+  const { getByTestId } = render(<App />);
+  const todoInput = getByTestId("todoInput");
+  const form = getByTestId("form");
+  const todoList = getByTestId("list");
+
+  fireEvent.change(todoInput, { target: { value: "task 1" } });
+  fireEvent.submit(form);
+
+  const listItem = within(todoList).getByText("task 1");
+  expect(listItem).toBeDefined();
+  expect(todoInput).toHaveValue("");
+});
+
+test("if listitem has buttons named mark as complete and delete", async () => {
+  const { getByTestId } = render(<App />);
+  const todoInput = getByTestId("todoInput");
+  const form = getByTestId("form");
+
+  fireEvent.change(todoInput, { target: { value: "task 1" } });
+  fireEvent.submit(form);
+
+  const listItem = getByTestId("listItem");
+  const markAsCompleteButton = await within(listItem).findByText(
+    "Mark as complete"
+  );
+  const deleteButton = await within(listItem).findByText("Delete");
+
+  expect(markAsCompleteButton).toBeDefined();
+  expect(deleteButton).toBeDefined();
+});
+
+test("if completed is appended to list item text if mark as complete is clicked and if mark as incomplete button is present", async () => {
+  const { getByTestId } = render(<App />);
+  const todoInput = getByTestId("todoInput");
+  const form = getByTestId("form");
+
+  fireEvent.change(todoInput, { target: { value: "task 1" } });
+  fireEvent.submit(form);
+
+  const listItem = getByTestId("listItem");
+  const markAsCompleteButton = await within(listItem).findByText(
+    "Mark as complete"
+  );
+
+  fireEvent.click(markAsCompleteButton);
+  const itemText = getByTestId("itemText");
+
+  const markAsInCompleteButton = await within(listItem).findByText(
+    "Mark as incomplete"
+  );
+
+  expect(markAsInCompleteButton).toBeDefined();
+  expect(itemText).toHaveTextContent("completed");
+});
+
+test("if listitem is removed when delete button is clicked", async () => {
+  const { getByTestId, queryByTestId } = render(<App />);
+  const todoInput = getByTestId("todoInput");
+  const form = getByTestId("form");
+
+  fireEvent.change(todoInput, { target: { value: "task 1" } });
+  fireEvent.submit(form);
+
+  let listItem = getByTestId("listItem");
+  const deleteButton = await within(listItem).findByText("Delete");
+
+  fireEvent.click(deleteButton);
+  listItem = queryByTestId("listItem");
+  expect(listItem).toBeNull();
 });
